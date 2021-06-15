d933268a32e6f5950be0736f12fc339e1d33bd3f
migarcia curro <migarcia@curro>
2021-06-15 14:53:02 +0200

    7.3

---
+++ get_next_line/gnlkiller-master/get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+int			get_next_line(int fd, char **line);
+size_t		ft_strlen(const char *s);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strdup(const char *s1);
+char        *ft_strchr(const char *s, int c);
+
+#endif
