38a1cc49b0b9705651d3b74b6a5638a36b0ff0fe
Mirgc <82157764+Mirgc@users.noreply.github.com>
2021-07-26 12:25:22 +0200

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,29 +1,50 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
-void check_flag(char *s, char *str)
+void ft_putchar(char c)
 {
+        write(0, &c, 1);
+}
 
+void ft_putstr(char *str_arg)
+{
+        while(*str_arg)
+        {
+                ft_putchar(*str_arg);
+                str_arg++;
+        }
+}
+
+void check_flag(char s, va_list arg)
+{
+        if (s == 'c')
+                ft_putchar((char)va_arg(arg, int));
+        if (s == 's')
+                ft_putstr(va_arg(arg, char *));
+        if (s == 'p')
+                write(0, va_arg(arg, unsigned long long), 16);
 }
 
 int ft_printf(const char *orig, ...)
 {
-	va_list arg;
-	char	*s;
-	char	*str;
-	va_start(arg, orig);
+        va_list arg;
+        char    *str;
+        va_start(arg, orig);
 
-	s = (char *)orig;
-	str = va_arg(arg, char *);
-	while (*s)
-	{
-		if (*s == '%')
-		{
-			s++;
-			check_flag(s, str);
-		}
-		write (0, s, 1);
-		s++;
-	}
-	return (0);
+        str = (char *)orig;
+        while (*str)
+        {
+                if (*str == '%')
+                {
+                        str++;
+                        check_flag(*str, arg);
+                        str++;
+                }
+                ft_putchar(*str);
+                str++;
+        }
+        va_end(arg);
+        free(str);
+        return (0);
 }
