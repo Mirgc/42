0107f8b81d3448b87a44ffd8bef25584b14c1ecd
Mirgc <82157764+Mirgc@users.noreply.github.com>
2021-07-26 12:25:56 +0200

    Update main1.c

--- ft_printf/main1.c
+++ ft_printf/main1.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
 int ft_printf(const char *s, ...);
 int main()
 {
-	char prueba[]= "Primera";
-	ft_printf("Hola esto es un caracter %c", prueba);
+        char prueba[]= "Primera";
+        ft_printf("Hola esto es un caracter '%c', '%c'\n", prueba[1], prueba[2]);
+        ft_printf("Hola esto es un string '%s'\n", prueba);
+        ft_printf("Hola esto es un puntero '%p'\n", prueba);
+        printf("Hola esto es un caracter %c\n", prueba[1]);
 }
