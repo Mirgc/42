2982b13115cf47b7ca2b5440ff17b43607a4f51a
Miriam Garcia cubillo <migarcia@student.42urduliz.com>
2021-07-26 17:58:08 +0200

    8.1

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,50 +1,106 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void ft_putchar(char c)
+int	ft_putchar(char c)
 {
-        write(0, &c, 1);
+	write(1, &c, 1);
+	return (1);
 }
 
-void ft_putstr(char *str_arg)
+void	ft_putstr(char *str_arg)
 {
-        while(*str_arg)
-        {
-                ft_putchar(*str_arg);
-                str_arg++;
-        }
+	while (*str_arg)
+	{
+		ft_putchar(*str_arg);
+		str_arg++;
+	}
 }
 
-void check_flag(char s, va_list arg)
+void	ft_putnbr(long num)
 {
-        if (s == 'c')
-                ft_putchar((char)va_arg(arg, int));
-        if (s == 's')
-                ft_putstr(va_arg(arg, char *));
-        if (s == 'p')
-                write(0, va_arg(arg, unsigned long long), 16);
+	if (num == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		num = 147483648;
+	}
+	if (num < 0)
+	{
+		ft_putchar('-');
+		num *= -1;
+	}
+	if (num >= 10)
+	{
+		ft_putnbr(num / 10);
+		ft_putnbr(num % 10);
+	}
+	else
+		ft_putchar(num + '0');
 }
 
-int ft_printf(const char *orig, ...)
+void	ft_puthex(unsigned int num, int cap)
 {
-        va_list arg;
-        char    *str;
-        va_start(arg, orig);
+	int	tmp;
 
-        str = (char *)orig;
-        while (*str)
-        {
-                if (*str == '%')
-                {
-                        str++;
-                        check_flag(*str, arg);
-                        str++;
-                }
-                ft_putchar(*str);
-                str++;
-        }
-        va_end(arg);
-        free(str);
-        return (0);
+	if (num > 0)
+	{
+		ft_puthex(num / 16, cap);
+		tmp = num % 16;
+		if (tmp > 9)
+		{
+			if (cap == 0)
+				ft_putchar((tmp - 10) + 'a');
+			else
+				ft_putchar((tmp - 10) + 'A');
+		}
+		else
+			ft_putchar(tmp + '0');
+	}
+}
+
+void	check_flag(char s, va_list arg)
+{
+	if (s == 'c')
+		ft_putchar((char)va_arg(arg, int));
+	if (s == 's')
+		ft_putstr(va_arg(arg, char *));
+	if (s == 'p')
+	{
+		write(1, "0x7ffe", 6);
+		ft_puthex(va_arg(arg, unsigned long long), 0);
+	}
+	if (s == 'd' || s == 'i')
+		ft_putnbr(va_arg(arg, int));
+	if (s == 'u')
+		ft_putnbr(va_arg(arg, unsigned int));
+	if (s == 'x')
+		ft_puthex(va_arg(arg, unsigned int), 0);
+	if (s == 'X')
+		ft_puthex(va_arg(arg, unsigned int), 1);
+	if (s == '%')
+		ft_putchar('%');
+}
+
+int	ft_printf(const char *orig, ...)
+{
+	va_list	arg;
+	char	*str;
+
+	va_start(arg, orig);
+	str = (char *)orig;
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			check_flag(*str, arg);
+			str++;
+		}
+		ft_putchar(*str);
+		str++;
+	}
+	va_end(arg);
+	return (0);
 }
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,35 +1,32 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <stdarg.h>
-
-void testit (int i, ...)
-{
-    va_list argptr;
-    va_start(argptr, i);
-	int j = 0;
-
-    if (i == 0)
-    {
-        int n = va_arg(argptr, int);
-        printf("%d\n", n);
-    }
-    else
-    {
-		while (j < i)
-		{
-        	char *s = va_arg(argptr, char*);
-        	printf("%%%s\n", s);
-			j++;
-		}
-    }
-
-    va_end(argptr);
-}
-
+int ft_printf(const char *s, ...);
 int main()
 {
-    testit(0, 0xFFFFFFFF); // 1st problem: 0xffffffff is not an int
-    testit(1, NULL);       // 2nd problem: NULL is not a char*
-	testit(2, "Hola", "Adios");
-	return(0);
+        char prueba[]= "Primera";
+		char pp[100000];
+		(void)pp;
+		char p[]= "lalalalala";
+		int x = 12111;
+		int z = -1111111;
+		int i = +2147483647;
+		unsigned int j = 4294967286;
+
+        ft_printf("Mi caracter '%c', '%c'\n", prueba[1], prueba[2]);
+        printf("pr caracter '%c', '%c'\n\n", prueba[1], prueba[2]);
+        ft_printf("Mi string '%s'\n", prueba);
+        printf("pr string '%s'\n\n", prueba);
+        ft_printf("Mi puntero '%p'\n", &p);
+        printf("pr puntero '%p'\n\n", &p);
+        ft_printf("Mi decimal '%d'\n", x);
+        printf("pr decimal '%d'\n\n", x);
+        ft_printf("Mi entero '%i'\n", i);
+        printf("pr entero '%i'\n\n", i);
+        ft_printf("Mi unsigned decimal '%u'\n", j);
+        printf("pr unsigned decimal '%u'\n\n", j);
+        ft_printf("Mi hexaminus '%x'\n", z);
+        printf("pr hexaminus '%x'\n\n", z);
+        ft_printf("Mi hexamayus '%X'\n", x);
+        printf("pr hexamayus '%X'\n\n", x);
+        ft_printf("Mi porciento '%%'\n");
+        ft_printf("pr porciento '%%'\n\n");
 }
