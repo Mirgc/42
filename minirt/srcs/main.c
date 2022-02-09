2aa8cce669906f0b134c4d305be822b82f0562e1
Miriam debian <m.g.cubillo@gmail.com>
2022-02-09 16:08:04 +0000

    2.2

--- minirt/srcs/main.c
+++ minirt/srcs/main.c
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void     draw(t_map *map, int x, int y)
+/*static void     draw(t_map *map, int x, int y)
 {
         int             point;
 
@@ -27,34 +27,114 @@ static void     draw(t_map *map, int x, int y)
                 map->image.data[point + 2] = 0xFF;
                 map->image.data[point + 3] = 0xFF;
         }
+}*/
+
+void    draw(t_map *map, int x, int y, int color)
+{
+        char    *dst;
+
+        dst = map->image.data + (y * map->image.size + x * (map->image.bpp / 8));
+        *(unsigned int*)dst = color;
 }
+
+int     get_trgb(int r, int g, int b)
+{
+	return ((r << 16) | (g << 8) | (b));
+}
+
 
 int	map_draw(t_map *map)
 {
 	int	w;
 	int	h;
+        int     vs_x;
+        int     vs_y;
+	int color = 0;
 	t_tup		p;
+	(void)w;
+	(void)h;
+	(void)vs_x;
+	(void)vs_y;
+	(void)color;
 
-	p = v_create(275, 450, 0, 1);
-			//p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_multi(m_scaling(5, 5, 5), m_rotationx(30))), p);
-			printf("%f, %f\n", p.x, p.y);
+	p = v_create(0, 0, 1, 1);
+	printf("%f, %f, %f\n", p.x, p.y, p.z);
+	p = m_multi_tup(m_scaling(0,0,5), p);
+	printf("%f, %f, %f\n", p.x, p.y, p.z);
+	//p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_multi(m_scaling(5, 5, 5), m_rotationx(30))), p);
 	map->mlx.img = mlx_new_image(map->mlx.init, P_WIDTH, P_HEIGHT);
 	map->image.data = mlx_get_data_addr(map->mlx.img, &map->image.bpp, \
                         &map->image.size, &map->image.endian);
-	h = 0;
+	color = get_trgb(255, 200, 200);
+/*	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	draw(map, 450 - p.x, 225 - p.z, color);
+			printf("%f, %f\n", p.x, p.y);*/
+	draw(map, 450 - p.x, 225 - p.z, color);
+	p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+	while ((int)p.x != 0)
+	{
+		p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+		draw(map, 450 - p.x, 225 - p.z, color);
+	}
+//	double d;
+/*	h = 0;
 	while (h < P_WIDTH)
 	{
+//                vs_y = h - P_WIDTH / 2;
 		w = 0;
 		while (w < P_HEIGHT)
 		{
-			p = m_multi_tup(m_multi(m_scaling(5, 5, 5), m_rotationx(30)), p);
-			printf("%f, %f\n", p.x, p.y);
-			if ((int)p.x == w && (int)p.y == h)	
-				draw(map, h, 550 - w);
+			vs_x = (h - P_WIDTH / 2) - p.x;
+		        vs_y = (w - P_HEIGHT / 2) - p.y;
+//                        vs_x = w - P_HEIGHT / 2;
+//			if (((int)p.x == h && (int)p.z == w) || ((int)p.x == 450 - h && (int)p.z == 225 - w))	
+//			{
+			if (((int)p.x == (h - 450) && (int)p.z == (w - 225)))	
+			//if (d > 0)
+			{
+				printf("%f, %i : %f, %i\n", p.x, h-450 , p.z, w-225);
+				color = get_trgb(200, 200, 200);
+				//draw(map, 450 - h, 225 - w);
+                        	draw(map, h, w, color);
+				p = m_multi_tup(m_multi(m_translation(10, 5, 7), m_rotationy(30)), p);
+			}
 			w++;
 		}
 		h++;
-	}
+	}*/
 	mlx_put_image_to_window(map->mlx.init, map->mlx.win, map->mlx.img, 0, 0);
 	mlx_destroy_image(map->mlx.init, map->mlx.img);
 	return (0);
@@ -70,8 +150,10 @@ int	main(int argc, char **argv)
 //        if (argc == 2)
 	map.mlx.init = mlx_init();
 	map.mlx.win = mlx_new_window(map.mlx.init, P_WIDTH, P_HEIGHT, "MINIRT");
+	map_draw(&map);
 //            mlx_hook(map->mlx.win, 2, 3, map_keys, map);
-	mlx_loop_hook(map.mlx.init, map_draw, &map);
+//            mlx_hook(map.mlx.win, 2, 3, map_draw, &map);
+//	mlx_loop_hook(map.mlx.init, map_draw, &map);
 	mlx_loop(map.mlx.init);
 
 //        }
