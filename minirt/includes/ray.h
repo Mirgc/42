0afebc8700e735cf30a18a9da7765b2b772271cf
Miriam debian <m.g.cubillo@gmail.com>
2022-02-16 15:56:00 +0000

    minirt 2.6

--- minirt/includes/ray.h
+++ minirt/includes/ray.h
@@ -4,24 +4,32 @@
 typedef struct s_ray
 {
 	t_tup	ori;
-	t_tup	dst;
+	t_tup	dir;
 } t_ray;
 
-typedef struct s_obj
+typedef struct s_sphere
 {
+	t_tup	center;
 	char	id;
-	float	t;
-} t_obj;
+	float	r;
+} t_sphere;
 
 typedef struct s_inter
 {
-	int			count;
-	float		point[2];
 	float		t;
-	t_obj		o;
+	t_sphere	o;
 } t_inter;
 
-t_tup	r_position(t_ray a, float i);
-t_inter	r_intersect(t_obj o, t_ray ray);
-t_inter	r_intersection(float t, char id);
+typedef struct s_arr_inter
+{
+	int	count;
+	t_inter	*a;
+} t_arr_inter;
+
+t_tup		r_position(t_ray a, float i);
+t_sphere	r_create_sphere();
+t_arr_inter	r_intersect(t_sphere s, t_ray ray);
+t_inter		r_intersection(float t, char id);
+t_arr_inter	r_intersections(t_inter i1, t_inter i2);
+float		hit(t_arr_inter inter);
 #endif
--- minirt/srcs/ray.c
+++ minirt/srcs/ray.c
@@ -2,41 +2,47 @@
 #include "ray.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 t_tup	r_position(t_ray a, float i)
 {
-	return (v_add(a.ori, v_multi(a.dst, i)));
+	return (v_add(a.ori, v_multi(a.dir, i)));
 }
 
-t_inter	r_intersect(t_obj obj, t_ray ray)
+t_sphere	r_create_sphere()
 {
-	t_tup	sp_to_ray;
+	t_sphere	s;
+	
+	s.center = v_create(0,0,0,1);
+	s.id = 's';
+	s.r = 0;
+	return (s);
+}
+
+t_arr_inter	r_intersect(t_sphere s, t_ray ray)
+{
+	t_tup		sp_to_ray;
 	double	a;
 	double	b;
 	double	c;
 	double	i;
-	t_inter	inter;
-	double	s;
-	t_tup	p;
+	t_inter	t1;
+	t_inter	t2;
 
-	inter.o.id = obj.id;
-	p = v_create(0,0,0,1);
-	sp_to_ray = v_substract(ray.ori, p);
-	a = v_dot(ray.dst, ray.dst);
-	b = 2 * v_dot(ray.dst, sp_to_ray);
+	sp_to_ray = v_substract(ray.ori, s.center);
+	a = v_dot(ray.dir, ray.dir);
+	b = 2 * v_dot(ray.dir, sp_to_ray);
 	c = v_dot(sp_to_ray, sp_to_ray) - 1;
 	i = powf(b, 2) - (4 * a * c);
-	printf("%f, %f, %f, %f\n", a, b, c, i);
+	t1.t = 0;
+	t1.t = 0;
 	if (i < 0)
-	{
-		inter.count = 0;
-		return (inter);
-	}
-	s = sqrtf(i);
-	inter.count = 2;
-	inter.point[0] = (-b - s) / (2 * a);
-	inter.point[1] = (-b + s) / (2 * a);
-	return (inter);
+		return (r_intersections(t1, t2));
+	i = sqrtf(i);
+	t1 = r_intersection((-b - i) / (2 * a), s.id);
+	t2 = r_intersection((-b + i) / (2 * a), s.id);
+	
+	return (r_intersections(t1, t2));
 }
 
 t_inter	r_intersection(float t, char id)
@@ -48,4 +54,33 @@ t_inter	r_intersection(float t, char id)
 	return (inter);
 }
 
+t_arr_inter r_intersections(t_inter i1, t_inter i2)
+{
+	t_arr_inter arr;
+
+	if (i1.t == 0 && i2.t == 0)
+	{
+		arr.count = 0;
+		return (arr);
+	}
+	arr.a = (t_inter *)malloc(sizeof(t_inter) * 2);
+	arr.count = 2;
+	arr.a[0] = i1;
+	arr.a[1] = i2;
+	return(arr);
+}
+
+float	r_hit(t_arr_inter inter)
+{
+	if (inter.a[0].t > 0 && inter.a[1] > 0)
+		return (fmin(inter.a[0].t, inter.a[1]));
+	if (inter.a[0].t > 0 || inter.a[1] > 0)
+		return (fmax(inter.a[0].t, inter.a[1]));
+	return(-1);
+}
+
+t_ray	r_transform(t_ray a, t_matrix m)
+{
+	
+}
 
