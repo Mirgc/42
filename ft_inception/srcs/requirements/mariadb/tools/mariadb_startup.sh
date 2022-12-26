7146e202a5dff57bce2001ea8fd9b94313b65f9d
Miriam debian <m.g.cubillo@gmail.com>
2022-12-26 13:39:43 +0000

    ft_inception done

---
+++ ft_inception/srcs/requirements/mariadb/tools/mariadb_startup.sh
@@ -0,0 +1,22 @@
+echo "Creating WP DB"
+if [ ! -d /var/lib/mysql/${DB_NAME} ]; then
+	service mysql start
+    	until mysqladmin ping; do
+		sleep 2
+	done
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+	mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'"
+	mysql -u root -e "FLUSH PRIVILEGES"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+	mysql -u root -e "FLUSH PRIVILEGES"
+
+	sed -i "s|bind-address|#bind-address|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+	cat /etc/mysql/mariadb.conf.d/50-server.cnf |grep "bind-add"
+else
+	echo "Wordpress database already exits"
+fi
+
+killall mysqld
+#mysqld_safe is the recommended way to start a mysqld server on Unix
+/usr/bin/mysqld_safe
